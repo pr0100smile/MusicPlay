2526c124dca398b13c0bdb6985e97a5810fe3934
Dmitriy NetoIT <pr0100_smile@mail.ru>
2023-08-20 16:59:54 +0300

    Rework file "MusicPlayer_select.sql"

--- MusicPlayer_select.sql
+++ MusicPlayer_select.sql
@@ -18,36 +18,58 @@ WHERE playlist_year BETWEEN 2018 AND 2020;
 SELECT artist_name FROM artists
 WHERE artist_name NOT LIKE '%% %%';
 
+
+-- REWORK
 -- 5.Название треков, которые содержат слово «мой» или «my»
+-- Version_1
 SELECT track_name FROM tracks
-WHERE track_name LIKE '%%My%%';
+WHERE track_name ILIKE 'My %%'
+OR track_name ILIKE '%% My'
+OR track_name ILIKE '%% My %%'
+OR track_name ILIKE 'My';
+
+-- Version_2
+SELECT track_name FROM tracks
+WHERE string_to_array(lower(track_name),' ')&& ARRAY['my','мой'];
+
+-- Version_3
+SELECT track_name FROM tracks
+WHERE track_name ~* '\m(my|мой)\M';
 
 -- TASK 3
-
 -- 1.Количество исполнителей в каждом жанре
 SELECT genre_name, count(artist_id) FROM music_genre
 JOIN artist_genre ON music_genre.id = artist_genre.genre_id
 GROUP BY genre_name;
 
+
+-- REWORK
 -- 2.Количество треков, вошедших в альбомы 2019–2020 годов
-SELECT album_year, count(track_name) FROM album
+SELECT count(track_name) FROM album
 JOIN tracks ON album.id = tracks.album_id
-WHERE album.album_year >= 2019 AND album.album_year <= 2020
-GROUP BY album_year;
+WHERE album_year BETWEEN 2019 AND 2020;
+
 
 -- 3.Средняя продолжительность треков по каждому альбому
 SELECT album_name, avg(track_duration) FROM album
 JOIN tracks ON album.id = tracks.album_id
 GROUP BY album_name;
 
--- 4.Все исполнители, которые не выпустили альбомы в 2020 году
-SELECT artists, album_year FROM album
-JOIN artist_album ON album.id = artist_album.album_id
-JOIN artists ON artist_album.artist_id = artists.id
-WHERE album.album_year != 2020;
 
+-- REWORK
+-- 4.Все исполнители, которые не выпустили альбомы в 2020 году
+SELECT DISTINCT artist_name FROM artists
+WHERE artist_name NOT IN (
+	SELECT artists.artist_name FROM artists
+	JOIN artist_album ON artists.id = artist_album.artist_id
+	JOIN album ON artist_album.album_id = album.id
+	WHERE album_year = 2020
+);
+
+
+-- REWORK
 -- 5.Названия сборников, в которых присутствует конкретный исполнитель (выберите его сами)
-SELECT playlist_name FROM playlist
+SELECT DISTINCT playlist_name FROM playlist
 JOIN tracks_playlist ON playlist.id = tracks_playlist.playlist_id
 JOIN tracks ON tracks_playlist.tracks_id = tracks.id
 JOIN album ON tracks.album_id = album.id
